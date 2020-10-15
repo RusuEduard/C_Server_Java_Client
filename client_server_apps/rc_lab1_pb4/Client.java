d1c41d7b15d32d08f0c27b55c9b08790844380e6
Rusu Eduard Constantin <rusu00eduard@gmail.com>
2020-10-15 12:02:38 +0300

    newProblem

---
+++ client_server_apps/rc_lab1_pb4/Client.java
@@ -0,0 +1,39 @@
+import java.net.*;
+import java.io.*;
+
+public class Client{
+    public static void main(String[] args) throws IOException {
+        String address = args[0];
+        int port = Integer.parseInt(args[1]);
+        System.out.println(address);
+        System.out.println(port);
+        
+        Socket c = new Socket(address, port);
+        
+        int len1, len2;
+        String s1, s2;
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        DataInputStream cIn = new DataInputStream(c.getInputStream());
+        DataOutputStream cOut = new DataOutputStream(c.getOutputStream());
+
+        System.out.println("Read the 1st string:");
+        s1 = reader.readLine();
+        len1 = s1.length();
+
+        cOut.writeShort(len1+1);
+        cOut.writeBytes(s1 + '\0');
+        cOut.flush();
+
+        System.out.println("Read the second string: ");
+        s2 = reader.readLine();
+        len2 = s2.length();
+
+        cOut.writeShort(len2 + 1);
+        cOut.writeBytes(s2 + '\0');
+        cOut.flush();
+        
+        c.close();
+    }
+}
---
+++ client_server_apps/rc_lab1_pb4/Server.c
@@ -0,0 +1,98 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <netinet/in.h>
+#include <signal.h>
+
+int handle_client(int c){
+    u_int16_t len1, len2;
+
+    recv(c, &len1, sizeof(len1), MSG_WAITALL);
+    len1 = ntohs(len1);
+    printf("[SERVER] Length of 1st string is: %d\n", len1);
+    char s1[len1];
+    recv(c, s1, len1, MSG_WAITALL);
+    printf("[SERVER] 1st string is: %s\n", s1);
+
+    recv(c, &len2, sizeof(len2), MSG_WAITALL);
+    len2 = ntohs(len2);
+    printf("[SERVER] Length of 2nd string is: %d\n", len2);
+    char s2[len2];
+    recv(c, s2, len2, MSG_WAITALL);
+    printf("[SERVER] 2nd string is: %s\n", s2);
+    int len3 = len1 + len2;
+    char s3[len3];
+    int k=0;
+    int i=0;
+    if(len1 <= len2){
+        while(k<len1-1){
+            s3[i] = s1[k];
+            s3[i+1] = s2[k];
+            k++;
+            i+=2;
+        }
+        while(k<len2){
+            s3[i] = s2[k];
+            k++;
+            i++;
+        }
+    }
+    else{
+        while(k<len2-1){
+            s3[i] = s1[k];
+            s3[i+1] = s2[k];
+            k++;
+            i+=2;
+        }
+        while(k<len1){
+            s3[i] = s1[k];
+            k++;
+            i++;
+        }
+    }
+
+    printf("[SERVER] New string is: %s\n", s3);
+    close(c);
+}
+
+int main(int argc, char *argv[]){
+    int s, c, l;
+    struct sockaddr_in server, client;
+    
+    printf("argv[0]: %s\n", argv[0]);
+    printf("argv[1]: %s\n", argv[1]);
+    char* ip = argv[1];
+    int port = atoi(argv[2]);
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = inet_addr(ip);
+
+    if(bind(s, (struct sockaddr*) &server, sizeof(server)) < 0){
+        printf("Eroare la bind!\n");
+        return(1);    
+    }
+
+
+    listen(s, 5);
+    l = sizeof(client);
+    memset(&client, 0, sizeof(client));
+    while(1){
+        c = accept(s, (struct sockaddr*) &client, &l);
+        printf("Client connected!\n");
+        if(fork() == 0){
+            handle_client(c);
+            exit(0);
+        }
+        signal(SIGCHLD, SIG_IGN);
+    }
+    close(s);
+    return(0);
+}
